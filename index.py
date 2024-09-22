5ee7f0c924a9cf637629b729ecb691a2432236ad
can <mhmtcndmr01@gmail.com>
2024-09-22 14:34:49 +0300

    add sum func

--- index.py
+++ index.py
@@ -1,2 +1,13 @@
-print('hello world')
-print('ali efe manap')
+print("hello world")
+print("ali efe manap")
+
+
+def sum(a, b):
+    print(a + b)
+
+
+a = input("Num 1: ")
+b = input("Num 2: ")
+
+
+sum(a, b)
