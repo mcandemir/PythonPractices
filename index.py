f16f196f4dfd83f1c94f65b2d8c31e2187c8e64e
can <mhmtcndmr01@gmail.com>
2024-09-30 14:31:34 +0300

    add multiplication func

--- index.py
+++ index.py
@@ -12,9 +12,15 @@ b = int(input("Num 2: "))
 
 sum(a, b)
 
-def sub(a,b):
+
+def sub(a, b):
     print(a - b)
 
+
 a = int(input("Num 1:"))
-b = int(input("Num 2:")) 
-sub(a,b)
+b = int(input("Num 2:"))
+sub(a, b)
+
+
+def mul(a, b):
+    print(a * b)
